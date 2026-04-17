931c5521c26e7281028be632d91abd73a7af14f6
dmakarau <denis.makarow@gmail.com>
2026-04-17 20:38:30 +0200

    wire config, SOAP client, and router in main

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/drebedengi-rest/internal/config"
+	"github.com/drebedengi-rest/internal/handlers"
+	"github.com/drebedengi-rest/internal/soap"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	soapClient := soap.NewClient(cfg.APIId, cfg.Login, cfg.Password, cfg.SoapURL)
+
+	h := &handlers.Handler{
+		SOAP:     soapClient,
+		APIId:    cfg.APIId,
+		Login:    cfg.Login,
+		Password: cfg.Password,
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(contentTypeJSON)
+
+	r.Mount("/api/v1", handlers.NewRouter(h))
+
+	log.Printf("Listening on :%s", cfg.Port)
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func contentTypeJSON(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
