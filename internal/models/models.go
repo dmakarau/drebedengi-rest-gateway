f1e8285a62014d1376c5bc2cbcd3df5c3e173872
dmakarau <denis.makarow@gmail.com>
2026-04-17 20:38:08 +0200

    add account and error models

---
+++ internal/models/models.go
@@ -0,0 +1,30 @@
+package models
+
+type AccessStatus struct {
+	Status int `json:"status"`
+}
+
+type CurrentRevision struct {
+	Revision int `json:"revision"`
+}
+
+type ExpireDate struct {
+	ExpireDate string `json:"expire_date"`
+}
+
+type SubscriptionStatus struct {
+	Status string `json:"status"`
+}
+
+type RightAccess struct {
+	Access string `json:"access"`
+}
+
+type UserID struct {
+	UserID string `json:"user_id"`
+}
+
+type APIError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
